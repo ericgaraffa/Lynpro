4914e474d81c5e8b54226af3d0cb2aaa2246320d
Thomas Leitao <thomas.leitao@itesoft.com>
2023-01-12 22:14:21 +0100

    add Home.js

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ import Personnaliser from './Personnaliser';
 
 const { height, width } = Dimensions.get('window');
 const buttonHeight = 50;
-
+//git
 const MyComponent = () => {
 
     const tempsPassage = [
