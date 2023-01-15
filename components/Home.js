186ec20934680cdecac8e0f02dcb54cbd3bf2cfb
Thomas Leitao <thomas.leitao@itesoft.com>
2023-01-15 22:56:33 +0100

    add style button

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,11 @@
 import React, {useRef, useState, useCallback, useEffect} from 'react';
-import {View, ScrollView, Text, Button, TouchableOpacity, Dimensions} from 'react-native';
+import {View, ScrollView, Text, Button, TouchableOpacity, Dimensions, TouchableHighlight, StyleSheet} from 'react-native';
 import Personnaliser from './Personnaliser';
 import {useNavigation} from "@react-navigation/native";
 
 const {height, width} = Dimensions.get('window');
-const buttonHeight = 50;
-//commit
+
+
 const MyComponent = () => {
 
     const navigation = useNavigation();
@@ -19,17 +19,17 @@ const MyComponent = () => {
     const [scroll, setScroll] = useState(false);
     const [persoCompet, setPersoCompet] = useState(false);
     const [click, setClick] = useState(false);
-    const [standardPos, setstandardPos] = useState({x: 0, y: height / 1.5});
-    const [classicPos, setclassicPos] = useState({x: 0, y: height + buttonHeight});
-    const [personaliserPos, setPersonaliserPos] = useState({x: 0, y: height + buttonHeight});
+    const [standardPos, setstandardPos] = useState({x: 0, y: height+ height/3.2});
+    const [classicPos, setclassicPos] = useState({x: 0, y: 0});
+    const [personaliserPos, setPersonaliserPos] = useState({x: 0, y: height*2 + height/50});
     const scrollViewRef = useRef(null);
-    const initialPosition = height / 2;
+    const initialPosition = height / 3;
     const [SelectedPassageProp, setSelectedPassageProp] = useState(null);
     const [SelectedIdProp, setSelectedIdProp] = useState(null);
 
     useEffect(() => {
         setstandardPos({x: 0, y: initialPosition});
-        setclassicPos({x: 0, y: initialPosition + buttonHeight});
+        setclassicPos({x: 0, y: initialPosition + height / 20 + height});
     }, [])
 
     const selectAutoClassic = () => {
@@ -48,18 +48,13 @@ const MyComponent = () => {
         setScroll(true);
     }, []);
 
-    const handleStandardPress = useCallback(() => {
+    const handlePersonnaliserPress = useCallback(() => {
         setPersoCompet(true);
         scrollViewRef.current.scrollTo({x: standardPos.x, y: standardPos.y, animated: true});
     }, []);
 
-    const handlePersonnaliserPress = useCallback(() => {
-        setPersoCompet(true);
-        scrollViewRef.current.scrollTo({x: classicPos.x, y: classicPos.y, animated: true});
-    }, []);
-
     const handleCompetPress = useCallback(() => {
-        setPersonaliserPos({x: 0, y: classicPos.y + buttonHeight});
+        setPersonaliserPos({x: 0, y: classicPos.y + height / 20});
         scrollViewRef.current.scrollTo({
             x: personaliserPos.x,
             y: personaliserPos.y - height / 2 + height / 1.3,
@@ -70,7 +65,7 @@ const MyComponent = () => {
     }, []);
 
     const handleClassicPress = useCallback(() => {
-        setPersonaliserPos({x: 0, y: classicPos.y + buttonHeight});
+        setPersonaliserPos({x: 0, y: classicPos.y + height / 20});
         scrollViewRef.current.scrollTo({
             x: personaliserPos.x,
             y: personaliserPos.y - height / 2 + height / 1.3,
@@ -84,7 +79,7 @@ const MyComponent = () => {
         setScroll(false)
         setPersoCompet(false)
         setstandardPos({x: 0, y: initialPosition});
-        setclassicPos({x: 0, y: initialPosition + buttonHeight});
+        setclassicPos({x: 0, y: initialPosition + height / 20});
         scrollViewRef.current.scrollTo({x: 0, y: 0, animated: true});
     }, [])
 
@@ -92,8 +87,8 @@ const MyComponent = () => {
         <View style={{flex: 1}}>
 
             {!scroll &&
-                <TouchableOpacity style={{alignSelf: 'center', marginTop: height / 2.5}} onPress={handleStartPress}>
-                    <Text>BATTLE</Text>
+                <TouchableOpacity style={styles.mainButton} onPress={handleStartPress}>
+                    <Text style={styles.mainButtonText}>BATTLE</Text>
                 </TouchableOpacity>}
             {scroll &&
                 <TouchableOpacity style={{position: 'absolute', top: 10, left: 10, zIndex: 1}}
@@ -104,21 +99,41 @@ const MyComponent = () => {
             <ScrollView scrollEnabled={false} ref={scrollViewRef}>
                 <View style={{height: '100%', width, backgroundColor: '#f0f0f0', padding: 10}}>
                     <View style={{alignSelf: 'center', marginTop: initialPosition}}>
-                        <Button title="STANDARD" onPress={() => navigation.navigate("Game", {
-                            wordTime: 4100,
-                            gameTime : 60,
-                            player1 : "player1",
-                            player2 : "player2"
-                        })}/>
-                        <Button title="PERSONNALISER" onPress={handleStandardPress}/>
+                        <TouchableHighlight
+                            style={styles.button}
+                            onPress={() => navigation.navigate("Game", {
+                                wordTime: 4100,
+                                gameTime : 60,
+                                player1 : "player1",
+                                player2 : "player2"
+                            })}
+                        >
+                            <Text style={styles.buttonText}>STANDARD</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight
+                            style={styles.button}
+                            onPress={handlePersonnaliserPress}
+                        >
+                            <Text style={styles.buttonText}>PERSONNALISER</Text>
+                        </TouchableHighlight>
                         {persoCompet && <View style={{
                             alignSelf: 'center',
-                            marginTop: height / 2.5,
+                            marginTop: height,
                             display: persoCompet ? 'flex' : 'none',
                             alignItems: 'center'
                         }}>
-                            <Button title="CLASSIC" onPress={handleClassicPress}/>
-                            <Button title="COMPET" onPress={handleCompetPress}/>
+                            <TouchableHighlight
+                                style={styles.button}
+                                onPress={handleClassicPress}
+                            >
+                                <Text style={styles.buttonText}>CLASSIC</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight
+                                style={styles.button}
+                                onPress={handleCompetPress}
+                            >
+                                <Text style={styles.buttonText}>COMPET</Text>
+                            </TouchableHighlight>
                             <View style={{
                                 alignSelf: 'center',
                                 marginTop: height / 2,
@@ -138,3 +153,31 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#000',
+        borderRadius: 25,
+        marginBottom: 50,
+        padding: 10,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    mainButton: {
+        position: 'absolute',
+        alignSelf: 'center',
+        marginTop: height / 2.5,
+        backgroundColor: '#000',
+        padding: 15,
+        borderRadius: 25
+    },
+    mainButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 30
+    }
+});
