9e13c574a70c41ffc4c415af30017ed3af7a68dd
ega <eric.garaffa@itesoft.com>
2023-04-12 11:15:09 +0200

    Update button width again

--- components/Format_choice.js
+++ components/Format_choice.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 1, // IOS
         shadowRadius: 1, //IOS
         elevation: 1, //Android (don't work)
-        width: width / 1.4,
+        width: width / 1.2,
         backgroundColor: 'yellow',
         borderRadius: 25,
         marginBottom: 50,
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'yellow',
         borderRadius: 25,
         marginBottom: 50,
-        width: width / 1.4,
+        width: width / 1.2,
         padding: 10,
         marginTop: 20,
         alignItems: 'center',
